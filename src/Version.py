d94826c994f5bd0ae9b4a9c73bdf5455c0bc7ff1
root <swmaniacster@gmail.com>
2023-08-24 11:34:14 +0200

    7.0.0

--- src/Version.py
+++ src/Version.py
@@ -21,6 +21,6 @@
 
 PLUGIN = "tmdb"
 ID = "TMDB"
-VERSION = "6.7.5"
+VERSION = "7.0.0"
 COPYRIGHT = "2018-2023 by dream-alpha"
 LICENSE = "This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version."
